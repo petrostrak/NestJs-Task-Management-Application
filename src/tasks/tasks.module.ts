0cb3ca554479ed4cb44c85e7fa38d983cf28c881
petros <pit.trak@gmail.com>
2020-09-12 13:57:39 +0300

    Created a task repository

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskRepository } from './task.repository';
 
 @Module({
+  imports: [
+    // We provide an array of entities or repositories to be included in
+    // the ecosystem of this module.
+    TypeOrmModule.forFeature([TaskRepository])
+  ],
   controllers: [TasksController],
   providers: [TasksService],
 })
