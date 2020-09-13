ef9167df11af0da932f100634ebb54f8b86f1009
petros <pit.trak@gmail.com>
2020-09-13 19:30:42 +0300

    Added validation AuthCredentialsDto, password strength with Regex

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,22 @@
+import { IsString, MinLength, MaxLength, Matches } from "class-validator"
+
 export class AuthCredentialsDto {
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/, { message: 'Password to weak' })
     password: string
 }
+
+/*
+    (?=.*[a-z])	The string must contain at least 1 lowercase alphabetical character
+    (?=.*[A-Z])	The string must contain at least 1 uppercase alphabetical character
+    (?=.*[0-9])	The string must contain at least 1 numeric character
+    (?=.*[!@#$%^&*]) The string must contain at least one special character
+*/
