661be200d939bd666eab6a0f2e8df51856b8f418
petros <pit.trak@gmail.com>
2020-09-14 19:10:59 +0300

    Added production configurations

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common'
 import { AppModule } from './app.module';
+import * as config from 'config'
+import { log } from 'console';
 
 async function bootstrap() {
-  const logger = new Logger('bootstrap')
+  const serverConfig = config.get('server')  
 
+  const logger = new Logger('bootstrap')
   const app = await NestFactory.create(AppModule);
-  const port = 3000
+
+  const port = process.env.PORT || serverConfig.port
   await app.listen(port);
   logger.log(`Application is listening on port ${port}`)
 }
