c452b997c2bf0a61dacd13c80432a5b2576df839
petros <pit.trak@gmail.com>
2020-09-14 12:04:24 +0300

    Added Authorization for createTask - User

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -3,6 +3,7 @@ import { Task } from "./task.entity";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { TaskStatus } from "./task-status.enums";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
+import { User } from "src/auth/user.entity";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
@@ -24,7 +25,7 @@ export class TaskRepository extends Repository<Task> {
         return tasks
     }
 
-    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
         // Create task via Entity
         const { title, description } = createTaskDto
         const task = new Task()
@@ -32,8 +33,11 @@ export class TaskRepository extends Repository<Task> {
         task.title = title
         task.description = description
         task.status = TaskStatus.OPEN
+        task.user = user
                 
         await task.save()
+
+        delete task.user
         
         return task
     }
