e4832df3f367e9f0c16720ae0b0274a211ca9f66
petros <pit.trak@gmail.com>
2020-09-14 12:46:55 +0300

    Fixed a bug in Custom Decorator where req.user was returning undefined

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -1,6 +1,7 @@
-import { createParamDecorator } from "@nestjs/common";
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { User } from "./user.entity";
 
-export const GetUser = createParamDecorator((data, req): User =>{
+export const GetUser = createParamDecorator<User>((data, ctx: ExecutionContext) =>{
+    const req = ctx.switchToHttp().getRequest()
     return req.user
 }) 
